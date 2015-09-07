7c54d7c3db475226f28bc04999ca7d29f6c27256
Leonel Quinteros <leonel.quinteros@gmail.com>
2015-09-07 11:51:04 -0300

    Improve router tests

--- router_test.go
+++ router_test.go
@@ -2,6 +2,7 @@ package yarf
 
 import (
 	"testing"
+	"net/url"
 )
 
 // Empty handler
@@ -15,6 +16,7 @@ func TestRouterRootMatch(t *testing.T) {
 
 	// Create empty context
 	c := new(Context)
+	c.Params = url.Values{}
 
 	// Create route
 	r := Route("/", h)
@@ -36,6 +38,7 @@ func TestRouterRootUnmatch(t *testing.T) {
 
 	// Create empty context
 	c := new(Context)
+	c.Params = url.Values{}
 
 	// Create route
 	r := Route("/", h)
@@ -47,6 +50,182 @@ func TestRouterRootUnmatch(t *testing.T) {
 	for _, s := range rs {
 		if r.Match(s, c) {
 			t.Errorf("'%s' shouldn't match against '/'", s)
+		}
+	}
+}
+
+func TestRouter1LevelMatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/level", h)
+
+	// Matching routes
+	rs := []string{"/level", "level"}
+
+	// Check
+	for _, s := range rs {
+		if !r.Match(s, c) {
+			t.Errorf("'%s' should match against '/level'", s)
+		}
+	}
+}
+
+func TestRouter1LevelUnmatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/level", h)
+
+	// Non-matching routes
+	rs := []string{"/", "", "/:level", "/Level", "/some/thing", "some/thing", "/more/levels/to/be/sure/it/shouldn't/matter/", "/with/trailer/"}
+
+	// Check
+	for _, s := range rs {
+		if r.Match(s, c) {
+			t.Errorf("'%s' shouldn't match against '/level'", s)
+		}
+	}
+}
+
+func TestRouterMultiLevelMatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/a/b/c", h)
+
+	// Matching routes
+	rs := []string{"/a/b/c", "a/b/c", "/a/b/c/", "a/b/c/"}
+
+	// Check
+	for _, s := range rs {
+		if !r.Match(s, c) {
+			t.Errorf("'%s' should match against '/a/b/c", s)
+		}
+	}
+}
+
+func TestRouterMultiLevelUnmatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/a/b/c", h)
+
+	// Non-matching routes
+	rs := []string{"/", "", "/:a/b/c", "/A/B/C", "/some/thing", "some/thing", "/more/levels/to/be/sure/it/shouldn't/matter", "///", "/almost/trailer/"}
+
+	// Check
+	for _, s := range rs {
+		if r.Match(s, c) {
+			t.Errorf("'%s' shouldn't match against '/a/b/c'", s)
+		}
+	}
+}
+
+func TestRouter1LevelParamMatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/:param", h)
+
+	// Matching routes
+	rs := []string{"/a", "a", "/cafewafewa", "/:paramStyle", "/trailer/"}
+
+	// Check
+	for _, s := range rs {
+		if !r.Match(s, c) {
+			t.Errorf("'%s' should match against '/:param'", s)
+		}
+	}
+}
+
+func TestRouter1LevelParamUnmatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/:param", h)
+
+	// Non-matching routes
+	rs := []string{"/", "", "/some/thing", "some/thing", "/more/levels/to/be/sure/it/shouldn't/matter/", "/with/trailer/"}
+
+	// Check
+	for _, s := range rs {
+		if r.Match(s, c) {
+			t.Errorf("'%s' shouldn't match against '/:param'", s)
+		}
+	}
+}
+
+func TestRouterMultiLevelParamMatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+
+	// Create route
+	r := Route("/a/b/:param", h)
+
+	// Matching routes
+	rs := []string{"/a/b/c", "a/b/c", "/a/b/c/", "a/b/c/", "/a/b/:c", "/a/b/:param"}
+
+	// Check
+	for _, s := range rs {
+		if !r.Match(s, c) {
+			t.Errorf("'%s' should match against '/a/b/:param'", s)
+		}
+	}
+}
+
+func TestRouterMultiLevelParamUnmatch(t *testing.T) {
+	// Create empty handler
+	h := new(Handler)
+
+	// Create empty context
+	c := new(Context)
+	c.Params = url.Values{}
+	
+	// Create route
+	r := Route("/a/b/:param", h)
+
+	// Non-matching routes
+	rs := []string{"/", "", "/a/b", "a/b", "/a/b/c/d", "/a/b/"}
+
+	// Check
+	for _, s := range rs {
+		if r.Match(s, c) {
+			t.Errorf("'%s' shouldn't match against '/a/b/:param'", s)
 		}
 	}
 }
