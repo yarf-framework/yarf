05600719541d84bbf70a09c2e4c08c015eb4fd9f
Leonel Quinteros <leonel.quinteros@gmail.com>
2015-09-24 10:18:51 -0300

    Added static file server example

---
+++ examples/static/main.go
@@ -0,0 +1,44 @@
+// static example package demonstrates how to easily implement
+// a yarf handler that serves static files using the net/http package.
+package main
+
+import (
+	"github.com/yarf-framework/yarf"
+	"net/http"
+)
+
+// Define a simple resource
+type Static struct {
+	yarf.Resource // Extend the yarf.Resource by composition
+
+	path string // Directory to serve static files from.
+}
+
+// Implement the static files handler
+func (s *Static) Get() error {
+	http.FileServer(http.Dir(s.path)).ServeHTTP(s.Context.Response, s.Context.Request)
+
+	return nil
+}
+
+// StaticDir constructs a Static handler and sets the path to serve under the route.
+func StaticDir(path string) *Static {
+	s := new(Static)
+	s.path = path
+
+	return s
+}
+
+// Entry point of the executable application
+// It runs a default server listening on http://localhost:8080
+func main() {
+	// Create a new empty YARF server
+	y := yarf.New()
+
+	// Add routes/resources
+	y.Add("/", StaticDir("/tmp"))
+	y.Add("/test", StaticDir("/var/www/test"))
+
+	// Start server listening on port 8080
+	y.Start(":8080")
+}
