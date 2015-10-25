5df4bda45100d58875b9b537591b20320c3766ef
Leonel Quinteros <leonel.quinteros@gmail.com>
2015-10-25 12:48:17 -0300

    Make golint happy

--- cache.go
+++ cache.go
@@ -20,12 +20,14 @@ type Cache struct {
 	sync.RWMutex
 }
 
+// NewCache creates and initializes a new Cache service object.
 func NewCache() *Cache {
 	return &Cache{
 		storage: make(map[string]routeCache),
 	}
 }
 
+// Get retrieves a routeCache object by key name.
 func (c *Cache) Get(k string) (rc routeCache, ok bool) {
 	c.RLock()
 	defer c.RUnlock()
@@ -34,6 +36,7 @@ func (c *Cache) Get(k string) (rc routeCache, ok bool) {
 	return
 }
 
+// Set stores a routeCache object under a key name.
 func (c *Cache) Set(k string, r routeCache) {
 	c.Lock()
 	defer c.Unlock()
